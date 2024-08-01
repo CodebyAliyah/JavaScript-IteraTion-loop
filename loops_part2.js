70297dcd387c150cb687300513b4bc5738428308
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-08-01 04:57:04 -0700

    Code Done

---
+++ loops_part2.js
@@ -0,0 +1,46 @@
+
+
+// while loop 
+
+let index = 0 ;
+while (index<=24) {
+  console.log(`value of index of is : ${index}`)
+  index ++ ;
+}
+
+
+
+
+let myArray = ["Quaid e Azam" , "Illama Iqbal" , "Sadar Saddam" , "Imran Khan"] 
+let arr = 0;
+while (arr < myArray.length) {
+  console.log(`value is ${myArray[arr]}`)
+  arr++ ;
+}
+
+
+// do while 
+
+// let scorre = 1 ;
+// do {
+//   console.log(`value is ${scorre}`)
+//   scorre++
+// }  while(scorre<=15)
+
+
+let scorre = 16 ;
+do {
+  console.log(`value is ${scorre}`)
+  scorre++
+}  while(scorre<=15)
+
+
+
+
+
+
+
+
+
+
+
